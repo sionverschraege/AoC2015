8b33f01edce4f512f1f8248140345080372ba249
sionverschraege123 <sion.verschraege@hogent.be>
2025-02-10 16:49:01 +0100

    Day 15 part 2

--- Day15/part2.py
+++ Day15/part2.py
@@ -1,5 +1,6 @@
 import time
 from pathlib import Path
+from functools import reduce
 
 def getInput(filename):
     path = Path(__file__)
@@ -10,8 +11,25 @@ def getInput(filename):
     return lines
 
 def solve(lines):
-    # Code goes here
-    return 'Not solved yet'
+    ingredients = [(int(c[:-1]),int(d[:-1]),int(f[:-1]),int(t[:-1]),int(cal)) for _,_,c,_,d,_,f,_,t,_,cal in [l.split() for l in lines]]
+    return max([reduce(lambda a,b: a*b if a > 0 and b > 0 else 0, [sum(ing[i] for ing in option) for i in range(4)]) for option in getOptions(ingredients, 0, 100, 500, {})])
+
+def getOptions(ingredients, ni, n, cals, cache):
+    if n == 0 and cals == 0:
+        return [[]]
+    elif ni >= len(ingredients) or n <= 0 or cals <= 0:
+        return []
+    elif (ni,n,cals) in cache.keys():
+        return cache[(ni,n,cals)]
+    else:
+        options = []
+        for nni in [ni, ni+1]:
+            if nni < len(ingredients):
+                i = ingredients[nni]
+                if i[4] <= cals:
+                    options += [[i] + so for so in getOptions(ingredients, nni, n - 1, cals - i[4], cache)]
+        cache[(ni,n,cals)] = options
+        return options
 
 for type in ["test","real"]:
     input = getInput(f'{type}input.txt')
