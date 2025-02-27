e2d6b023ea098d10af7f48bdef0baa8394e0a9e2
sionverschraege123 <sion.verschraege@hogent.be>
2025-02-27 14:32:06 +0100

    Bad attempt at 19.2

--- Day19/part2.py
+++ Day19/part2.py
@@ -9,15 +9,76 @@ def getInput(filename):
         lines[i] = lines[i].removesuffix('\n')
     return lines
 
+def toMolecule(s: str) -> str:
+    m = ''
+    for c in s:
+        if c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZe':
+            if m != '':
+                m += '-'
+            m += c
+        else:
+            m += c
+    return m
+
 def solve(lines):
-    replacements = {}
+    itoo = {}
+    otoi = {}
     for line in lines[:-2]:
-        o,_,i = line.split()
-        if i not in replacements.keys():
-            replacements[i] = []
-        replacements[i].append(o)
+        i,_,o = line.split()
+        i,o = (toMolecule(i),toMolecule(o)) 
+        if i not in itoo.keys():
+            itoo[i] = []
+        itoo[i].append(o)
+        if o not in otoi.keys():
+            otoi[o] = []
+        otoi[o].append(i)
+    notJustAtStart = set()
+    maybeJustAtStart = set()
+    for key in otoi.keys():
+        atom = key.split('-')[0]
+        if atom not in notJustAtStart:
+            maybeJustAtStart.add(atom)
+        for atom in key.split('-')[1:]:
+            notJustAtStart.add(atom)
+            maybeJustAtStart.discard(atom)
+    for key in itoo.keys():
+        atom = key.split('-')[0]
+        if atom not in notJustAtStart:
+            maybeJustAtStart.add(atom)
+    changed = True
+    while changed:
+        changed = False
+        toSwitch = set()
+        for n in notJustAtStart:
+            if n in itoo.keys():
+                for tg in itoo[n]:
+                    toSwitch.add(tg.split('-')[0])
+        if maybeJustAtStart.intersection(toSwitch):
+            changed = True
+            notJustAtStart = notJustAtStart.union(toSwitch)
+            maybeJustAtStart = maybeJustAtStart.difference(toSwitch)
     
-    
+    justAtStart = maybeJustAtStart
+
+    options = {toMolecule(lines[-1])}
+    steps = 0
+    while 'e' not in options:
+        steps += 1
+        nopts = set()
+        for o in options:
+            for i in range(len(o)):
+                for di in range(15):
+                    if o[i:i+di] in otoi.keys():
+                        for tg in otoi[o[i:i+di]]:
+                            if i == 0 or tg.split('-')[0] not in justAtStart:
+                                nopt = o[:i] + tg + o[i+di:]
+                                if nopt == 'e' or nopt[0] != 'e':
+                                    nopts.add(nopt)
+        options = nopts
+    return steps
+    # lastStep = (lines[-1],0,None)
+    # while lastStep[0] != 'e':
+
 
 for type in ["test","real"]:
     input = getInput(f'{type}input.txt')
